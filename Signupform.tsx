26cefeb358586a55b591ad17e8c926314210e70c
Shaik-siddiq <77263229+Shaik-siddiq@users.noreply.github.com>
2024-01-17 20:14:30 +0530

    Updated error validations

---
+++ Signupform.tsx
@@ -0,0 +1,118 @@
+import React from 'react';
+import { useForm } from 'react-hook-form';
+import {
+  Button,
+  FormControl,
+  InputLabel,
+  MenuItem,
+  Select,
+  TextField,
+  Radio,
+  RadioGroup,
+  FormControlLabel,
+  FormHelperText,
+} from '@mui/material';
+
+interface FormData {
+  [key: string]: string;
+}
+
+interface FieldProps {
+  label: string;
+  name: string;
+  type: string;
+  options?: string[];
+  defaultValue?: string;
+  required?: boolean;
+  minLength?: number;
+  maxLength?: number;
+  customValidation?: (value: string) => string | undefined;
+
+}
+
+interface SignupFormProps {
+  formData: FieldProps[];
+}
+
+const SignupForm: React.FC<SignupFormProps> = ({ formData }) => {
+  const {
+    register,
+    handleSubmit,
+    setValue,
+    formState: { errors },
+  } = useForm<FormData>();
+
+  const onSubmit = (data: FormData) => {
+    console.log(data);
+  };
+
+  const renderField = (field: FieldProps) => {
+    const { label, name, type, options, defaultValue, required, minLength, maxLength, customValidation } = field;
+  
+    const commonProps = {
+      ...register(name, {
+        required: required? `please enter proper ${label}`:false,
+        minLength: minLength ? { value: minLength, message: `${label} must be at least ${minLength} characters` } : undefined,
+        maxLength: maxLength ? { value: maxLength, message: `${label} must not exceed ${maxLength} characters` } : undefined,
+        validate: customValidation ? (value) => customValidation(value) : undefined,
+      }),
+      defaultValue: defaultValue,
+    };
+  
+    switch (type) {
+      case 'TEXT':
+        return (
+          <div>
+            <TextField fullWidth label={label} variant="outlined" {...commonProps} />
+            <FormHelperText error={!!errors[name]}>{errors[name]?.message}</FormHelperText>
+          </div>
+        );
+      case 'LIST':
+        return (
+          <FormControl fullWidth variant="outlined">
+            <InputLabel>{label}</InputLabel>
+            <Select {...commonProps}>
+              {options &&
+                options.map((option: string, index: number) => (
+                  <MenuItem key={index} value={option}>
+                    {option}
+                  </MenuItem>
+                ))}
+            </Select>
+            <FormHelperText error={!!errors[name]}>{errors[name]?.message}</FormHelperText>
+          </FormControl>
+        );
+      case 'RADIO':
+        return (
+          <FormControl component="fieldset">
+            <label>{label}</label>
+            <RadioGroup row aria-label={label} name={name} defaultValue={defaultValue}>
+              {options &&
+                options.map((option: string, index: number) => (
+                  <FormControlLabel key={index} value={option} control={<Radio />} label={option} />
+                ))}
+            </RadioGroup>
+            <FormHelperText error={!!errors[name]}>{errors[name]?.message}</FormHelperText>
+          </FormControl>
+        );
+      default:
+        return null;
+    }
+  };
+  
+
+  return (
+    <form onSubmit={handleSubmit(onSubmit)}>
+      {formData.map((field: FieldProps, index: number) => (
+        <div key={index} style={{ marginBottom: '16px' }}>
+          {renderField(field)}
+        </div>
+      ))}
+      <Button type="submit" variant="contained" color="primary">
+        Submit
+      </Button>
+    </form>
+  );
+};
+
+export default SignupForm;
---
+++ index.tsx
@@ -0,0 +1,66 @@
+import { Container, CssBaseline, Paper, Typography } from '@mui/material';
+import SignupForm from './Signupform';
+
+const formData = [
+  {
+    label: 'Full Name',
+    name: 'fullName',
+    type: 'TEXT',
+    required: true,
+    defaultValue: "John Doe",
+    minLength: 1,
+    maxLength: 100
+  },
+  {
+    label: 'Email',
+    name: 'email',
+    type: 'TEXT',
+    required: true,
+    defaultValue: "hello@mail.com",
+    minLength: 1,
+    maxLength: 50,
+    customValidation: (value: string) => {
+      if (!value.includes('@')) {
+        return 'Invalid email format';
+      }
+      return undefined;
+    },
+  },
+  {
+    label: 'Gender',
+    name: 'gender',
+    type: 'LIST',
+    options: ['Male', 'Female', 'Others'],
+    required: true,
+    defaultValue: '1'
+  },
+  {
+    label: 'Love React?',
+    name: 'loveReact',
+    type: 'RADIO',
+    defaultValue: '1',
+    required: false,
+    options: ['Yes', 'No'],
+  },
+
+];
+
+const Home: React.FC = () => {
+  return (
+    <Container component="main" maxWidth="sm">
+      <CssBaseline />
+      <Paper elevation={3} style={{ height: "38rem", padding: '20px', display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center' }}>
+        <Typography component="h1" variant="h5" style={{ marginBottom: '20px' }}>
+          Signup Form
+        </Typography>
+        <SignupForm formData={formData} />
+      </Paper>
+    </Container>
+  );
+};
+
+export default Home;
+
+
+
+
